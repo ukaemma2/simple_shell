9d749addd468a521dd55fc6ad33ac105837617c0
ukaemma2 <ukaemmanuel2000@gmail.com>
2022-12-16 11:50:09 -0800

    getline prompt

---
+++ shell.h
@@ -0,0 +1,13 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/*prototypes*/
+int _getline(void);
+
+
+#endif /*_SHELL_H_*/
+
