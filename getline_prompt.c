b84f7d47f6061709bcc59707376ccf017658e8e2
ukaemma2 <ukaemmanuel2000@gmail.com>
2022-12-28 13:31:07 -0800

    updates

--- getline_prompt.c
+++ getline_prompt.c
@@ -1,43 +1,161 @@
 #include "shell.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
- * _getline - this is the prompt and getline function for simple shell
- * Return: pointer to the getline data
+ * input_buf - buffers chained commands
+ * @info: parameter struct
+ * @buf: address of buffer
+ * @len: address of len var
+ *
+ * Return: bytes read
  */
 
-int *_getline(void)
+ssize_t input_buf(info_t *info, char **buf, size_t *len)
 {
 
-	size_t n = 0;
-	char *buff = NULL, *token, *delimeter = "\n";
-	struct stat st;
+	ssize_t r = 0;
+	size_t len_p = 0;
 
-	while (1)
-	{
-		buff = malloc(sizeof(char) * n);
-		if (buff != NULL)
+	if (!*len) /* if nothing left in the buffer, fill it */
+
+		free(*buf);
+		*buf = NULL;
+		signal(SIGINT, sigintHandler);
+#if USE_GETLINE
+		r = getline(buf, &len_p, stdin);
+#else
+		r = _getline(info, buf, &len_p);
+#endif
+		if (r > 0)
 		{
-			printf("emma_mavel_shell ");
-			if ((getline(&buff, &n, stdin)) == -1)
-				perror("Error with getline");
-
-			token = strtok(buff, delimeter);
-			if (stat(token, &st) == 0)
-				printf("\n%s: FOUND. Make a call to STRINGTOKENIZATION and EXECVE\n\n", token);
-			else
-				printf("\n%s: NOT FOUND. Loop\n\n", token);
-			if (strcmp(token, "exit") == 0)
+			if ((*buf)[r - 1] == '\n')
 			{
-				printf("\nExisting now\n");
-				token = NULL, free(token);
-				buff = NULL, free(buff);
-				break;
+				(*buf)[r - 1] = '\0'; /*remove trailing newline */
+				r--;
+			}
+			info->linecount_flag = 1;
+			remove_comments(*buf);
+			build_history_list(info, *buf, info->histcount++);
+			/* if (_strchr(*buf, ';')) is this a command chain? */
+			{
+				*len = r;
+				info->cmd_buf = buf;
 			}
 		}
-		token = NULL, free(token);
-		buff = NULL, free(buff);
 	}
-	return (0);
+	return (r);
 }
+/**
+ * get_input - gets a line minus the newline
+ * @info: parameter struct
+ *
+ * Return: bytes read
+ */
+ssize_t get_input(info_t *info)
+{
+	static char *buf; /* the ';' command chain buffer */
+	static size_t i, j, len;
+	ssize_t r = 0;
+	char **buf_p = &(info->arg), *p;
+
+	_putchar(BUF_FLUSH);
+	r = input_buf(info, &buf, &len);
+	if (r == -1) /* EOF */
+		return (-1);
+	if (len)	/* we have commands left in the chain buffer */
+	{
+		j = i; /* init new iterator to current buf position */
+		p = buf + i; /* get pointer for return */
+		check_chain(info, buf, &j, i, len);
+		while (j < len) /* iterate to semicolon or end */
+		{
+			if (is_chain(info, buf, &j))
+				break;
+			j++;
+		}
+		i = j + 1; /* increment past nulled ';'' */
+		if (i >= len) /* reached end of buffer? */
+		{
+			i = len = 0; /* reset position and length */
+			info->cmd_buf_type = CMD_NORM;
+		}
+		*buf_p = p; /* pass back pointer to current command position */
+		return (_strlen(p)); /* return length of current command */
+	}
+	*buf_p = buf; /* else not a chain, pass back buffer from _getline() */
+	return (r); /* return length of buffer from _getline() */
+}
+/**
+ * read_buf - reads a buffer
+ * @info: parameter struct
+ * @buf: buffer
+ * @i: size
+ *
+ * Return: r
+ */
+ssize_t read_buf(info_t *info, char *buf, size_t *i)
+{
+	ssize_t r = 0;
+
+	if (*i)
+		return (0);
+	r = read(info->readfd, buf, READ_BUF_SIZE);
+	if (r >= 0)
+		*i = r;
+	return (r);
+}
+/**
+ * _getline - gets the next line of input from STDIN
+ * @info: parameter struct
+ * @ptr: address of pointer to buffer, preallocated or NULL
+ * @length: size of preallocated ptr buffer if not NULL
+ *
+ * Return: s
+ */
+int _getline(info_t *info, char **ptr, size_t *length)
+{
+	static char buf[READ_BUF_SIZE];
+	static size_t i, len;
+	size_t k;
+	ssize_t r = 0, s = 0;
+	char *p = NULL, *new_p = NULL, *c;
+
+	p = *ptr;
+
+	if (p && length)
+		s = *length;
+	if (i == len)
+		i = len = 0;
+	r = read_buf(info, buf, &len);
+	if (r == -1 || (r == 0 && len == 0))
+		return (-1);
+	c = _strchr(buf + i, '\n');
+	k = c ? 1 + (unsigned int)(c - buf) : len;
+	new_p = _realloc(p, s, s ? s + k : k + 1);
+	if (!new_p) /* MALLOC FAILURE! */
+		return (p ? free(p), -1 : -1);
+	if (s)
+		_strncat(new_p, buf + i, k - i);
+	else
+		_strncpy(new_p, buf + i, k - i + 1);
+	s += k - i;
+	i = k;
+	p = new_p;
+	if (length)
+		*length = s;
+	*ptr = p;
+	return (s);
+}
+/**
+ * sigintHandler - blocks ctrl-C
+ * @sig_num: the signal number
+ *
+ * Return: void
+ */
+void sigintHandler(__attribute__((unused))int sig_num)
+{
+	_puts("\n");
+	_puts("$ ");
+	_putchar(BUF_FLUSH);
+}
+
+
